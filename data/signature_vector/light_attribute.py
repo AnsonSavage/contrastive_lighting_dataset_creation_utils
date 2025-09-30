c765912a3b5af0b68bf436734e18860400a0b0ae
AnsonSavage <ansonsavage@gmail.com>
2025-09-29 20:11:01 -0600

    Add dataclass definitions

--- data/signature_vector/light_attribute.py
+++ data/signature_vector/light_attribute.py
@@ -68,13 +68,13 @@ class ContrastLevel(Enum):
 required_enums_outdoor = set([IndoorOutdoor, NaturalArtificial, TimeOfDay, ContrastLevel])
 required_enums_indoor = set([IndoorOutdoor, NaturalArtificial, ContrastLevel]) # If you're indoor, you don't need time of day
 
-category_to_enum_maper = {
+category_to_enum_map = {
     'outdoor': IndoorOutdoor.OUTDOOR,
     'natural light': NaturalArtificial.NATURAL,
     'sunrise-sunset': TimeOfDay.SUNRISE_SUNSET,
     'low contrast': ContrastLevel.LOW,
     'indoor': IndoorOutdoor.INDOOR,
-    'artificial light': NaturalArtificial.ARTIFICIAL, # note, some are tagged with both natural and artificial light... Do we want an override?
+    'artificial light': NaturalArtificial.ARTIFICIAL, # NOTE, some are tagged with both natural and artificial light... Do we want an override?
     'morning-afternoon': TimeOfDay.MORNING_AFTERNOON,
     'high contrast': ContrastLevel.HIGH,
     'midday': TimeOfDay.MIDDAY,
@@ -82,11 +82,20 @@ category_to_enum_maper = {
     'night': TimeOfDay.NIGHT
 }
 
+@dataclass
 class HDRI(LightSourceAttribute):
-    pass
+    time_of_day: TimeOfDay | None
+    indoor_outdoor: IndoorOutdoor
+    natural_artificial: NaturalArtificial
+    contrast_level: ContrastLevel
+
 @dataclass
 class VirtualLight(LightSourceAttribute):
-    light_size: 'LightSize' = None
+    light_size: LightSize
+    light_direction: LightDirection
+    light_intensity: LightIntensity
+    light_color: LightColor
+
 class KeyLight(VirtualLight):
     pass
 class FillLight(VirtualLight):
@@ -94,8 +103,6 @@ class FillLight(VirtualLight):
 class RimLight(VirtualLight):
     pass
 
-import os
-import json
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List folders in an HDRI directory.")
     parser.add_argument("hdri_directory", type=str, help="Path to the HDRI directory")
@@ -107,7 +114,7 @@ if __name__ == "__main__":
         print(folder)
         json_path = os.path.join(hdri_directory, folder, f"{folder}_asset_metadata.json")
         categories = json.load(open(json_path))['info']['categories']
-        my_properties = [category_to_enum_maper[category] for category in categories if category in category_to_enum_maper]
+        my_properties = [category_to_enum_map[category] for category in categories if category in category_to_enum_map]
         # ensure we have all the required enums represented
         if IndoorOutdoor.INDOOR in [prop for prop in my_properties]:
             assert required_enums_indoor.issubset(set(type(prop) for prop in my_properties)), f"Missing required enums in {folder}: {required_enums_indoor - set(type(prop) for prop in my_properties)}"
